6a72567f7e9d5a1ad5a15c1d2a4facfdb21b617c
Сара Фурманова <sarafurmann@192.168.1.3>
2022-06-11 17:29:18 +0300

    GEO HW

--- geoReview.js
+++ geoReview.js
@@ -20,100 +20,40 @@ export default class GeoReview {
   }
 
   async callApi(method, body = {}) {
-    const res = await fetch(`/homework_8/${method}`, {
+    const res = await fetch(`/geo-review-3/${method}`, {
       method: 'post',
       body: JSON.stringify(body),
     });
     return await res.json();
   }
 
-  async insertAddress(coords, addressField) {
-    return (addressField.textContent = await this.map.getStreetNameWithClick(coords));
-  }
-
   createForm(coords, reviews) {
     const root = document.createElement('div');
     root.innerHTML = this.formTemplate;
-    const reviewList = root.querySelector('.reviews__list');
+    const reviewList = root.querySelector('.review-list');
     const reviewForm = root.querySelector('[data-role=review-form]');
     reviewForm.dataset.coords = JSON.stringify(coords);
 
     for (const item of reviews) {
-      const li = document.createElement('li');
-      li.classList.add('review__item');
-      li.innerHTML = `
-      <div class="review__heading"> 
-      <div class="review__author">${item.name}</div>
-      <div class="review__location">${item.place}</div>
-      <div class="review__date">${item.date}</div>
-      </div>
-      <div class="review__content">${item.text}</div>
-      `;
-      reviewList.appendChild(li);
+      const div = document.createElement('div');
+      div.classList.add('review-item');
+      div.innerHTML = `
+    <div>
+      <b>${item.name}</b> [${item.place}]
+    </div>
+    <div>${item.text}</div>
+    `;
+      reviewList.appendChild(div);
     }
 
     return root;
   }
 
   async onClick(coords) {
-    await this.map.openBalloon(coords, 'Загрузка...');
+    this.map.openBalloon(coords, 'Загрузка...');
     const list = await this.callApi('list', { coords });
     const form = this.createForm(coords, list);
     this.map.setBalloonContent(form.innerHTML);
-    const addressField = document.querySelector('[data-role=click-address]');
-    await this.insertAddress(coords, addressField);
-  }
-
-  getTodayDate() {
-    return new Date();
-  }
-
-  formatDate(date) {
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = '0' + hours;
-    }
-
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = '0' + minutes;
-    }
-
-    let day = date.getDate();
-    if (day < 10) {
-      day = '0' + day;
-    }
-
-    let month = date.getMonth();
-    if (month === 0) {
-      month = 'января';
-    } else if (month === 1) {
-      month = 'февраля';
-    } else if (month === 2) {
-      month = 'марта';
-    } else if (month === 3) {
-      month = 'апреля';
-    } else if (month === 4) {
-      month = 'мая';
-    } else if (month === 5) {
-      month = 'июня';
-    } else if (month === 6) {
-      month = 'июля';
-    } else if (month === 7) {
-      month = 'августа';
-    } else if (month === 8) {
-      month = 'сентября';
-    } else if (month === 9) {
-      month = 'октября';
-    } else if (month === 10) {
-      month = 'ноября';
-    } else {
-      month = 'декабря';
-    }
-
-    const year = date.getFullYear();
-
-    return `${hours}:${minutes} ${day} ${month} ${year} года`;
   }
 
   async onDocumentClick(e) {
@@ -126,7 +66,6 @@ export default class GeoReview {
           name: document.querySelector('[data-role=review-name]').value,
           place: document.querySelector('[data-role=review-place]').value,
           text: document.querySelector('[data-role=review-text]').value,
-          date: this.formatDate(this.getTodayDate()),
         },
       };
 
--- interactiveMap.js
+++ interactiveMap.js
@@ -1,3 +1,5 @@
+/* global ymaps */
+
 export default class InteractiveMap {
   constructor(mapId, onClick) {
     this.mapId = mapId;
@@ -26,7 +28,6 @@ export default class InteractiveMap {
 
   initMap() {
     this.clusterer = new ymaps.Clusterer({
-      preset: 'islands#invertedDarkOrangeClusterIcons',
       groupByCoordinates: true,
       clusterDisableClickZoom: true,
       clusterOpenBalloonOnClick: false,
@@ -38,55 +39,12 @@ export default class InteractiveMap {
     this.map = new ymaps.Map(this.mapId, {
       center: [55.76, 37.64],
       zoom: 10,
-      controls: [],
     });
-    this.map.controls.add('zoomControl');
-    this.map.controls.add('searchControl');
-    this.map.behaviors.disable(['dblClickZoom']);
     this.map.events.add('click', (e) => this.onClick(e.get('coords')));
     this.map.geoObjects.add(this.clusterer);
-    this.MyBalloonLayout = ymaps.templateLayoutFactory.createClass(
-      `<div class="pop-up">
-        <div class="header">
-          <div class="header__left">
-            <div class="map-marker map-marker--before-heading"></div>
-            <div class="address" data-role="click-address"></div>
-          </div>
-          <div class="header__right">
-            <div class="close-icon"></div>
-          </div>
-        </div>
-        <div class="main">
-          $[[options.contentLayout observeSize minWidth=235 maxWidth=235 maxHeight=550]]
-        </div>
-      </div>`,
-      {
-        build: function () {
-          this.map.constructor.superclass.build.call(this);
-          this.popup = document.querySelector('.pop-up');
-          this.close = document.querySelector('.close-icon');
-          this.close.addEventListener('click', {});
-        },
-      }
-    );
   }
 
-  getStreetNameWithClick(coords) {
-    return new Promise((resolve, reject) => {
-      ymaps
-        .geocode(coords)
-        .then((res) =>
-          resolve(
-            res.geoObjects.get(0)
-              ? res.geoObjects.get(0).getAddressLine()
-              : 'Не удалось определить адрес.'
-          )
-        )
-        .catch((e) => reject(e));
-    });
-  }
-
-  async openBalloon(coords, content) {
+  openBalloon(coords, content) {
     this.map.balloon.open(coords, content);
   }
 
@@ -99,16 +57,7 @@ export default class InteractiveMap {
   }
 
   createPlacemark(coords) {
-    const placemark = new ymaps.Placemark(
-      coords,
-      {
-        hintContent: 'Метка "Geo-review"',
-      },
-      {
-        preset: 'islands#darkOrangeDotIcon',
-        balloonLayout: this.MyBalloonLayout,
-      }
-    );
+    const placemark = new ymaps.Placemark(coords);
     placemark.events.add('click', (e) => {
       const coords = e.get('target').geometry.getCoordinates();
       this.onClick(coords);
